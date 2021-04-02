4ef9d80a38aed0578e4427d1ed3f5c21c7bf1b3d
Rogerio Alexandre Braga <rogerioabraga@hotmail.com>
2021-04-01 23:27:03 -0300

    Adicionada uma Linha no Código

--- TesteGitHub/src/IniciandoGitHub/GitHub.java
+++ TesteGitHub/src/IniciandoGitHub/GitHub.java
@@ -5,5 +5,6 @@ public class GitHub {
 	public static void main(String[] args) {
 		
 		System.out.println("Configuração GitHub Iniciada");
+		System.out.println("Configuração GitHub Finalizada");
 	}
 }
