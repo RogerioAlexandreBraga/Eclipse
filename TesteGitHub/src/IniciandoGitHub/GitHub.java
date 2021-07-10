5859597da8243bf82d41b89bd4a05f6ccc46ac4e
RogerioAlexandreBraga <81777231+RogerioAlexandreBraga@users.noreply.github.com>
2021-07-09 22:52:47 -0300

    Update GitHub.java

--- TesteGitHub/src/IniciandoGitHub/GitHub.java
+++ TesteGitHub/src/IniciandoGitHub/GitHub.java
@@ -4,9 +4,10 @@ public class GitHub {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Configuração GitHub Iniciada");
-		System.out.println("Configuração GitHub Finalizada");
-		System.out.println("Configuração Após Reparo do Windows");
-		System.out.println("Configuração Após Reinstalação");
+		System.out.println("ConfiguraÃ§Ã£o GitHub Iniciada");
+		System.out.println("ConfiguraÃ§Ã£o GitHub Finalizada");
+		System.out.println("ConfiguraÃ§Ã£o ApÃ³s Reparo do Windows");
+		System.out.println("ConfiguraÃ§Ã£o ApÃ³s ReinstalaÃ§Ã£o");
+		System.out.println("ConfiguraÃ§Ã£o Full");
 	}
 }
