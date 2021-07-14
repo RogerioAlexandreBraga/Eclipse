34939353f83226002364ad471f15c1a6870714b0
RogerioAlexandreBraga <rogerioabraga@hotmail.com>
2021-07-14 00:46:38 -0300

    Atualização05

--- TesteGitHub/src/IniciandoGitHub/GitHub.java
+++ TesteGitHub/src/IniciandoGitHub/GitHub.java
@@ -7,6 +7,6 @@ public class GitHub {
 		System.out.println("Atualização 01");
 		System.out.println("Atualização 02");
 		System.out.println("Atualização 03");
-		System.out.println("Atualização 04");
+
 	}
 }
